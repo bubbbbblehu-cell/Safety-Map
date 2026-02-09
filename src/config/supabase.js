36e33885577d34fdc45be4acce0207c36e871dce
bubbbbblehu-cell <bubbbbble.hu@gmail.com>
2026-02-09 22:23:58 +0800

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase 配置
 const SUPABASE_URL = 'https://hmmruoankhohowlzajll.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbXJ1b2Fua2hvaG93bHphamxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5OTI2NzAsImV4cCI6MjA1NDU2ODY3MH0.sb_publishable_c7LCnNKtl3HgKdXjUWaunw_K4fts9ji';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbXJ1b2Fua2hvaG93bHphamxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1MjQxNjgsImV4cCI6MjA4NjEwMDE2OH0.nGVgFlrTPQh86Ba0doT_mmeHvCnF0NjJ6MUJEvQKV3g';
 
 // 创建 Supabase 客户端
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
